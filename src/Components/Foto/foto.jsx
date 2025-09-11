eac15efda21c5019a142b7975ddf1ba3b124259f
silke-J <silk0776@mcdmstudent.dk>
2025-09-11 22:45:24 +0200

    imgbørn

--- src/Components/Foto/foto.jsx
+++ src/Components/Foto/foto.jsx
@@ -2,13 +2,15 @@ import styles from "./foto.module.css";
 import EC from "../../../public/engle-c.jpg";
 
 import CP from "../../../public/cute-par.jpg";
-
+import BL from "../../../public/børn.jpg";
 
 const Foto = () => {
   return (
     <>
       <section className={styles.body}>
+        <img className={styles.børn} src={BL} alt="" />
         <img className={styles.cute} src={CP} alt="" />
+
         <img className={styles.engle} src={EC} alt="" />
       </section>
     </>
